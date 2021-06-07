5a7072f74d4d0d3a0ea6ea48c8c5f4adde1dbf30
Darlan de Castro <darlandc@gmail.com>
2021-06-06 21:38:52 -0300

    wip bc

--- blockchain.js
+++ blockchain.js
@@ -1,16 +1,33 @@
 const Block = require('./block');
 
 class BlockChain {
-    constructor(){
+    constructor() {
         this.chain = [Block.genesis()];
     }
 
-    addBlock(data){
+    addBlock(data) {
         const block = Block.mineBlock(this.chain[this.chain.length - 1], data);
         this.chain.push(block);
 
         return block;
     }
+
+    isValidChain(chain) {
+        if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
+
+        for (let i = 0; i < chain.length; i++) {
+            const block = chain[i];
+            const lastBlock = chain[i - 1];
+        }
+
+        if (block.lastHash !== lastBlock.hash || block.hash !== Block.blockHash(block)) {
+            return false;
+        }
+        return true;
+
+
+    }
+
 }
 
 module.exports = BlockChain;
